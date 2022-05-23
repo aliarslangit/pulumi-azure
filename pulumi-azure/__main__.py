36cb285c2fdd9ca9be7d040231452db382b28b7b
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-05-23 15:33:09 +0500

    Update __main__.py

--- pulumi-azure/__main__.py
+++ pulumi-azure/__main__.py
@@ -12,17 +12,17 @@ import pulumi_azure_native as azure_native
 resource_group = resources.ResourceGroup('demo-pulumi-rg')
 
 # create ACR
-acr = resources.containerregistry.Registry("registry",
-                                           admin_user_enabled=True,
-                                           location=resource_group.location,
-                                           registry_name="myRegistryforcmppulumi",
-                                           resource_group_name=resource_group.name,
-                                           sku=azure_native.containerregistry.SkuArgs(
-                                               name="Standard",
-                                           ),
-                                           tags={
-                                               "key": "value",
-                                           })
+acr = azure_native.containerregistry.Registry("registry",
+                                              admin_user_enabled=True,
+                                              location=resource_group.location,
+                                              registry_name="myRegistryforcmppulumi",
+                                              resource_group_name=resource_group.name,
+                                              sku=azure_native.containerregistry.SkuArgs(
+                                                  name="Standard",
+                                              ),
+                                              tags={
+                                                  "key": "value",
+                                              })
 
 # # create MySQL
 # mysqlserver = azure.mysql.Server("exampleServer",
